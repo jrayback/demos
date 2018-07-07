276075f4f33508e7383952d6eac2fe938fc58337
Jonathan Rayback <jrayback@gmail.com>
2018-07-06 18:44:58 -0600

    Current version of entrypoint. Breaks on trying to make db connection.

--- Jolt/Rayback_Jonathan_JOLT_BE_Assessment/app/app.js
+++ Jolt/Rayback_Jonathan_JOLT_BE_Assessment/app/app.js
@@ -6,6 +6,10 @@ const db = require('./db')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use('teachers', require('./controllers/teachers'))
+app.use('classes', require('./controllers/classes'))
+app.use('books', require('./controllers/books'))
+app.use('students', require('./controllers/students'))
 
 const port = process.env.PORT || 8080
 
@@ -18,11 +22,19 @@ router.get('/', (req, res) => {
 
 // more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router)
 
-// START THE SERVER
-// =============================================================================
-app.listen(port)
-console.log('Listening on port ' + port)
+// Make DB connection and start the service
+// GETTING STUCK HERE. CAN'T MAKE THE DB CONNECTION. CAN'T CONNECT TO THE
+// CONTAINERIZED DB FROM MY LOCAL ENVIRO...I NEED TO SET UP A LOCAL DB, TROUBLESHOOT
+// AND THEN COME BACK TO FIGURE OUT THE CONNECTION PROBLEM IN DOCKER...
+db.connect((err) => {
+  if (err) {
+    console.log('Unable to connect to MySQL.')
+    process.exit(1)
+  } else {
+    app.listen(port)
+    console.log('Listening on port ' + port)
+  }
+})
